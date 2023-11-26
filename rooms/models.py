d767c6e3c50d7ab148224af3490a6739e578aa94
badacura <badacura@gmail.com>
2023-11-26 13:01:45 +0900

    feat: merge

--- rooms/models.py
+++ rooms/models.py
@@ -4,27 +4,30 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 
 class Room(models.Model):
-    name = models.CharField(max_length=20)
-    address = models.TextField()
-    image = models.TextField()
-    description = models.TextField()
-    capacity = models.IntegerField()
-    # reservation = models.ManyToManyField(get_user_model(), through='Reservation', related_name='reserv_room')
-    review = models.ManyToManyField(get_user_model(), through='Review', related_name='review_room')
+    room_name = models.CharField(max_length=50)
+    room_address_latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    room_address_longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    room_image = models.ImageField(blank=True, upload_to='%Y/%m/%d/')
+    room_des = models.TextField()
+    room_capacity = models.IntegerField()
+    room_reservation = models.ManyToManyField(get_user_model(), through='Reservation', related_name='user_room')
+    room_review = models.ManyToManyField(get_user_model(), through='Review', related_name='review_room')
+    room_created_at = models.DateTimeField(auto_now_add=True)
+    room_updated_at = models.DateTimeField(auto_now=True)
 
-# class Reservation(models.Model):
-#     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-#     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-#     check_in = models.DateField()
-#     check_out = models.DateField()
+class Reservation(models.Model):
+    reservation_user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='user_reservated')
+    reservation_room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='room_reservated')
+    reservation_check_in = models.DateField()
+    reservation_check_out = models.DateField()
 
 class Review(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    review_room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='reviewed_room')
-    rating = models.FloatField()
-    comment = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    review_user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='user_reviewed')
+    review_room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='room_reviewed')
+    review_rating = models.FloatField()
+    review_comment = models.TextField()
+    review_created_at = models.DateTimeField(auto_now_add=True)
+    review_updated_at = models.DateTimeField(auto_now=True)
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Room
+from django.response import Response
 
 # Create your views here.
+def index(requests):
+    rooms = Room.objects.all()
+    serializer = Room
