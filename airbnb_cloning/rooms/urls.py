10654da403a947f090a20f5c6df188f98058021e
Hanjinwoo <chococan16@gmail.com>
2023-12-14 13:48:30 +0900

    ee

--- airbnb_cloning/rooms/urls.py
+++ airbnb_cloning/rooms/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 app_name = 'rooms'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:room_id>/', views.detail, name='detail'),
-    path('reviews/', views.reviews, name='reviews'),
-    path('<int:room_id>/book/', views.book, name='book'),
-    # path('create/', views.create, name='create'),
-    # path('<int:pk>/delete/', views.delete, name='delete'),
-    # path('<int:pk>/update/', views.update, name='update'),
+    path('', views.index),
+    path('<int:room_id>/', views.detail),
+    path('reviews/', views.reviews),
+    path('<int:room_id>/book/', views.book),
+    # path('create/', views.create),
+    # path('<int:pk>/delete/', views.delete),
+    # path('<int:pk>/update/', views.update),
 ]
